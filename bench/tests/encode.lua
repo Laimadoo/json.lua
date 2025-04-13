27a499c2e9bd36e42c86b2e0b8da1ef214fdc6f4
Laimadoo Slimelime <72653939+Laimadoo@users.noreply.github.com>
2025-04-14 00:24:24 +0300

    Create tests/encode.lua

---
+++ bench/tests/encode.lua
@@ -0,0 +1,29 @@
+local benchmark = require("bench.benchmark")
+local lai = require("json")
+local rxi = require("bench.rxi_json")
+
+local data = {
+    firstName = "John",
+    lastName = "Smith",
+    isAlive = true,
+    age = 25,
+    address = {
+        streetAddress = "21 2nd Street",
+        city = "New York",
+        state = "NY",
+        postalCode = "10021-3100"
+    },
+    phoneNumbers = {
+        { type = "home", number = "212 555-1234" },
+        { type = "office", number = "646 555-4567" }
+    },
+    children = {},
+    spouse = nil
+}
+
+local r1 = benchmark(100, lai.encode, data)
+local r2 = benchmark(100, rxi.encode, data)
+print("time")
+print("lai: " .. r1 .. " rxi: " .. r2)
+print("diff lai - rxi")
+print(r1-r2)
