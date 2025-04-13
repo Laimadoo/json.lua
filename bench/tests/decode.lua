d896332ebee3e3d879f84fe82e878a895374e24f
Laimadoo Slimelime <72653939+Laimadoo@users.noreply.github.com>
2025-04-14 01:06:00 +0300

    Update decode.lua

--- bench/tests/decode.lua
+++ bench/tests/decode.lua
@@ -2,34 +2,39 @@ local benchmark = require("bench.benchmark")
 local lai = require("json")            -- github.com/laimadoo/json.lua
 local rxi = require("bench.rxi_json")  -- github.com/rxi/json.lua
 
-local text = [[{
-  "firstName": "John",
-  "lastName": "Smith",
-  "isAlive": true,
-  "age": 25,
-  "address": {
-    "streetAddress": "21 2nd Street",
-    "city": "New York",
-    "state": "NY",
-    "postalCode": "10021-3100"
-  },
-  "phoneNumbers": [
-    {
-      "type": "home",
-      "number": "212 555-1234"
+local text = "[" .. string.rep([[{
+    "firstName": "John",
+   "lastName": "Smith",
+    "isAlive": true,
+    "age": 25,
+    "address": {
+        "streetAddress": "21 2nd Street",
+        "city": "New York",
+        "state": "NY",
+        "postalCode": "10021-3100"
     },
-    {
-      "type": "office",
-      "number": "646 555-4567"
-    }
-  ],
-  "children": [],
-  "spouse": null
-}]]
+    "phoneNumbers": [
+        {
+            "type": "home",
+            "number": "212 555-1234"
+        },
+        {
+            "type": "office",
+            "number": "646 555-4567"
+        }
+    ],
+    "children": [],
+    "spouse": null
+}, ]], 1000) .. "]"
 
-local r1 = benchmark(100, lai.decode, text)
-local r2 = benchmark(100, rxi.decode, text)
+local r1 = benchmark(1, lai.decode, text)
+local r2 = benchmark(1, rxi.decode, text)
+
+local diff  = string.format("%.02f", r1-r2)
+local diffp = string.format("%.02f", (math.abs(r1/r2) - 1)*100)
+
+print("BENCHMARK [DECODE]")
 print("time")
 print("lai: " .. r1 .. " rxi: " .. r2)
 print("diff lai - rxi")
-print((r1-r2) .. "-- " .. (r1/r2) .. "%")
+print(diff .. " -- " .. diffp .. "%")
