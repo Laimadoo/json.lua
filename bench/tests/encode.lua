2cd25969e4b70f6e0a541b6915722c0c8f2995bc
Laimadoo Slimelime <72653939+Laimadoo@users.noreply.github.com>
2025-04-14 01:05:43 +0300

    Update encode.lua

--- bench/tests/encode.lua
+++ bench/tests/encode.lua
@@ -2,28 +2,36 @@ local benchmark = require("bench.benchmark")
 local lai = require("json")            -- github.com/laimadoo/json.lua
 local rxi = require("bench.rxi_json")  -- github.com/rxi/json.lua
 
-local data = {
-    firstName = "John",
-    lastName = "Smith",
-    isAlive = true,
-    age = 25,
-    address = {
-        streetAddress = "21 2nd Street",
-        city = "New York",
-        state = "NY",
-        postalCode = "10021-3100"
-    },
-    phoneNumbers = {
-        { type = "home", number = "212 555-1234" },
-        { type = "office", number = "646 555-4567" }
-    },
-    children = {},
-    spouse = nil
-}
+local data = {}
+for i = 1, 1000 do
+    data[i] = {
+        firstName = "John",
+        lastName = "Smith",
+        isAlive = true,
+        age = 25,
+        address = {
+            streetAddress = "21 2nd Street",
+            city = "New York",
+            state = "NY",
+            postalCode = "10021-3100"
+        },
+        phoneNumbers = {
+            { type = "home", number = "212 555-1234" },
+            { type = "office", number = "646 555-4567" }
+        },
+        children = {},
+        spouse = nil
+    }
+end
 
-local r1 = benchmark(100, lai.encode, data)
-local r2 = benchmark(100, rxi.encode, data)
+local r1 = benchmark(1, lai.encode, data)
+local r2 = benchmark(1, rxi.encode, data)
+
+local diff  = string.format("%.02f", r1-r2)
+local diffp = string.format("%.02f", (math.abs(r1/r2) - 1)*100)
+
+print("BENCHMARK [ENCODE]")
 print("time")
 print("lai: " .. r1 .. " rxi: " .. r2)
 print("diff lai - rxi")
-print((r1-r2) .. "-- " .. (r1/r2) .. "%")
+print(diff .. " -- " .. diffp .. "%")
